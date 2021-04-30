84ea8ce42d8ee4ef3ea4f986ec5f53755352cd0b
Brian Grant <45053941+BrianGrant-Data@users.noreply.github.com>
2021-04-30 12:24:34 -0600

    Create skill_tree.py
    This is my initial musing on a graphical representation of my skills. This might be a helpful document that can be used in addition to the resume itself.

---
+++ skill_tree.py
@@ -0,0 +1,20 @@
+'''
+Goal: 
+- Make a function that takes ~docs/skills.md (or .txt if I change it later) and plots it like a video game skill tree. 
+
+Needs:
+- a dataframe containing the following collumns:
+    - Index id
+    - General Tree Name (Group skills by the big over terms data scientist are expected to have ('math', 'programming', 'commincation', etc.))
+    - The skill's name itself
+    - A list of skills you believe were prerequisite for it
+    - Date you believe the skill was learned
+    - Date skill was actually documented as learned (in case you don't know when you learned the skill)
+ - it needs to be able to parse the list of skills and remove skills that were needed for other skills (show only the end most skills in the tree
+ - detect when the skill tree circles upon itself (ex: if then statements > for loops > if then statements)
+ - plot the dataframe as a visual skill tree
+ 
+ Why:
+ - As I record the skills I gain this might be a neat and informative representation of what I've learned.
+ - This also gives a road map for helping others to learn what I've learned. You could use it for article ideas to write about.
+'''
